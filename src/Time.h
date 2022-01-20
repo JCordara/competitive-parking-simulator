691d3859815dc699ee16e7de2d3fdd45861628cb
Joseph Cordara <josephcordara@gmail.com>
2022-01-20 14:26:38 -0700

    Add ImGui FPS counter

--- src/Time.h
+++ src/Time.h
@@ -1,32 +1,43 @@
 /** Time.h
- * Static class for accessing time-related values
- * now(): Current system time
- * delta():   Time elapsed since last frame
+ * Static class for maintaining time-related values
+ * now(): 	Current system time
+ * delta(): Time elapsed since last frame
+ * fps():	Current frames per second
 */
 
 #ifndef TIME_H
 #define TIME_H
 
+// Use the last FPS_FRAMES_COUNT frames to calculate current fps
+#define FPS_FRAMES_COUNT 25
+
 #include <GLFW/glfw3.h> // glfwGetTime()
+#include <cstring> 		// memset()
 
 class Time {
 public:
 
-	// Updates the current time and the time since last call
-	static void computeTimeDelta() {
-		Time::delta_ = glfwGetTime() - Time::lastFrameTime;
-		Time::lastFrameTime = glfwGetTime();
-	}
+	// Initialize some static values
+	static void init();
 
+	// Updates the current time and the time since last call
+	// Call every frame
+	static void update();
+
+	// Accessor functions
 	static double now() { return glfwGetTime(); }
 	static double delta() { return Time::delta_; }
+	static double fps() { return fps_; }
 
 private:
-	// Hide the default constructor to discourage instantiation
-	Time();
+	Time();	// Hide the default constructor to discourage instantiation
 
-	static double lastFrameTime;
-	static double delta_;
+	static double lastFrameTime_;			 // Time last frame was renedered
+	static double delta_;					 // Time taken to render last frame
+	static double fps_;						 // Current frames per second
+	static double deltas_[FPS_FRAMES_COUNT]; // Buffer of previous deltas
+	static double deltaSum_;				 // Sum of buffer entries
+	static unsigned int deltaIndex_;		 // Current buffer insertion index
 };
 
 #endif // TIME_H
