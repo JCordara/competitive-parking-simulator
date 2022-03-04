3025bfb68d4a2de349cda485506507a149254645
KeatonBuddy <sagorbanik@gmail.com>
2022-03-03 23:02:18 -0700

    jklasgb

--- src/PhysicsSystem.cpp
+++ src/PhysicsSystem.cpp
@@ -267,34 +267,12 @@ void PhysicsSystem::initPhysics()
 	boxShape3->release();
 
 	//Create Mesh Object
-	/*PxVec3 convexVerts[] = {PxVec3(0,1,0),PxVec3(1,0,0),PxVec3(-1,0,0),PxVec3(0,0,1),
+	PxVec3 convexVerts[] = {PxVec3(0,1,0),PxVec3(1,0,0),PxVec3(-1,0,0),PxVec3(0,0,1),
 	PxVec3(0,0,-1) };
 	auto mesh = scene->addEntity();
-	mesh->getComponent<PhysicsComponent>()->addActor();*/
+	mesh->getComponent<PhysicsComponent>()->addActor();
 
-	PxTransform mesht = PxTransform(PxVec3(0, 20.0f, 0.0f));
-	PxRigidDynamic* mesh1 = gPhysics->createRigidDynamic(mesht);
-	static const PxVec3 convexVerts[] = { PxVec3(0,1,0),PxVec3(1,0,0),PxVec3(-1,0,0),PxVec3(0,0,1),
-	PxVec3(0,0,-1) };
-	PxConvexMeshDesc convexDesc;
-	convexDesc.points.count = sizeof(convexVerts);
-	convexDesc.points.stride = sizeof(PxVec3);
-	convexDesc.points.data = convexVerts;
-	convexDesc.flags = PxConvexFlag::eCOMPUTE_CONVEX;
-
-	PxDefaultMemoryOutputStream buf;
-	PxConvexMeshCookingResult::Enum result;
-	if (!gCooking->cookConvexMesh(convexDesc, buf, &result)) {
-		return;
-	};
-
-	PxDefaultMemoryInputData input(buf.getData(), buf.getSize());
-	PxConvexMesh* convexMesh = gPhysics->createConvexMesh(input);
-	PxShape* meshShape = PxRigidActorExt::createExclusiveShape(*mesh1, PxConvexMeshGeometry(convexMesh), *gMaterial);
-	PxFilterData meshFilterData(COLLISION_FLAG_GROUND_AGAINST, COLLISION_FLAG_OBSTACLE, 0, 0);
-	meshShape->setSimulationFilterData(meshFilterData);
-	gScene->addActor(*mesh1);
-	meshShape->release();
+	
 
 	/*
 	PxTransform mesht = PxTransform(PxVec3(0, 20.0f, 0.0f));
